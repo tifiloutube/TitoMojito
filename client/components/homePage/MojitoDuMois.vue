cc94b41b2da777ded9ebbf8b66e33bd8297b46e9
Philippe VILARINHO <59869747+tifiloutube@users.noreply.github.com>
2024-03-20 09:33:40 +0100

    Update MojitoDuMois.vue

--- client/components/homePage/MojitoDuMois.vue
+++ client/components/homePage/MojitoDuMois.vue
@@ -93,7 +93,6 @@ onMounted(
   font-style: normal;
   font-weight: 700;
   line-height: normal;
-  background-image: url("https://images.unsplash.com/photo-1605027990121-cbae9e0642df?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"); /* imageBackground */
   background-size: cover;
   background-position: center;
   background-clip: text;
