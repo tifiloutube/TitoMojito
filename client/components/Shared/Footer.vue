44bbf0342e20ab5c013659a3e351e7bcf9c86d5b
Philippe VILARINHO <59869747+tifiloutube@users.noreply.github.com>
2024-04-09 14:03:46 +0200

    [UPDATE] - Update design footer responsive

--- client/components/Shared/Footer.vue
+++ client/components/Shared/Footer.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <footer>
+  <footer class="desktop">
     <div class="topContainer wrapper">
       <p>Menu</p>
       <p>Social</p>
@@ -39,9 +39,52 @@
     </div>
     <p class="copyright">Copyright © 2024 par TitoMojito. Tous droits réservés. Developpement & Design credits : <a class="linkdev" href="https://vilarinho-philippe.netlify.app/" target="_blank"> Philippe VILARINHO</a> </p>
   </footer>
+  <footer class="mobile">
+    <div>
+      <h3 class="h3">Menu</h3>
+      <span class="line"></span>
+      <ul class="menu">
+        <li><NuxtLink to="/" style="text-decoration: none; color: #2B2B2B;">Accueil</NuxtLink></li>
+        <li><NuxtLink to="/le-bar" style="text-decoration: none; color: #2B2B2B;">Le bar</NuxtLink></li>
+        <li><NuxtLink to="/equipe" style="text-decoration: none; color: #2B2B2B;">L'équipe</NuxtLink></li>
+        <li><NuxtLink to="/carte" style="text-decoration: none; color: #2B2B2B;">La carte</NuxtLink></li>
+      </ul>
+    </div>
+    <div>
+      <h3 class="h3">Social</h3>
+      <span class="line"></span>
+      <ul class="socialNetwork">
+        <li>
+          <a href="https://www.instagram.com/titomojito.toulouse/" target="_blank" style="text-decoration: none; color: #2B2B2B;">Instagram</a>
+        </li>
+        <li>
+          <a href="https://www.facebook.com/toulousetitomojito/?show_switched_toast=0&show_invite_to_follow=0&show_switched_tooltip=0&show_podcast_settings=0&show_community_review_changes=0&show_community_rollback=0&show_follower_visibility_disclosure=0" target="_blank" style="text-decoration: none; color: #2B2B2B;">Facebook</a>
+        </li>
+        <li>
+          <a href="https://www.tiktok.com/@titomojito.toulouse?_t=8k4dhueae09&_r=1" target="_blank" style="text-decoration: none; color: #2B2B2B;">TikTok</a>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <h3 class="h3">Contanct</h3>
+      <span class="line"></span>
+      <ul class="contact">
+        <li>
+          <a href="https://www.google.com/maps/place//data=!4m2!3m1!1s0x12aebbf86ffe08b1:0xe3ada0abbbee574c?sa=X&ved=1t:8290&ictx=111" style="text-decoration: none; color: #2B2B2B;" target="_blank">40 Rue Jean-Baptiste Merly, 31000 Toulouse</a>
+        </li>
+        <li>
+          <a href="mailto:info@titomojito.fr" style="text-decoration: none; color: #2B2B2B; word-break: break-word;">info@titomojito.fr</a>
+        </li>
+      </ul>
+    </div>
+    <p class="copyright">Copyright © 2024 par TitoMojito. Tous droits réservés. Developpement & Design credits : <a class="linkdev" href="https://vilarinho-philippe.netlify.app/" target="_blank"> Philippe VILARINHO</a> </p>
+  </footer>
 </template>
 
 <style scoped>
+.mobile {
+  display: none;
+}
 
 footer {
   margin-top: -90px;
@@ -134,14 +177,31 @@ NuxtLink {
 }
 
 @media screen and (max-width: 900px) {
-  p {
-    font-size: 20px;
+  .desktop {
+    display: none;
   }
-  a {
-    font-size: 15px;
-  }
-  .copyright {
-    font-size: 10px;
+  .mobile {
+    display: flex;
+    flex-direction: column;
+    gap: 40px;
+    .line {
+      margin-top: 15px;
+    }
+    .h3 {
+      font-size: 25px;
+      font-weight: 300;
+    }
+    li {
+      font-size: 20px;
+      font-weight: 300;
+    }
+    .h3, li {
+      padding-left: 15px;
+      padding-right: 15px;
+    }
+    .copyright {
+      margin-top: 30px;
+    }
   }
 }
 </style>
