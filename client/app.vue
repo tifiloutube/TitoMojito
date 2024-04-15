cdcf2271e5efae7536e8329b71f899dcde2b2986
Philippe VILARINHO <59869747+tifiloutube@users.noreply.github.com>
2024-04-15 15:24:48 +0200

    [FEAT] - création menu

--- client/app.vue
+++ client/app.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
+    <Menu />
     <NuxtPage />
     <Footer />
   </div>
 </template>
 <script setup lang="ts">
 import Footer from "~/components/Shared/Footer.vue";
+import Menu from "~/components/Shared/Menu.vue";
 </script>
