66a5274f4c4d0fdc09bc2b455dc1e8faa157c5d5
Philippe VILARINHO <59869747+tifiloutube@users.noreply.github.com>
2025-04-15 19:18:24 +0200

    SEO

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -17,9 +17,4 @@ export default defineNuxtConfig({
       apiUrl: process.env.API_URL,
     }
   },
-  sitemap: {
-    siteUrl: 'https://titomojito.fr',
-    gzip: true,
-    trailingSlash: false
-  }
 });
